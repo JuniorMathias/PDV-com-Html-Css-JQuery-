8b26d5331a3bdb04e8edfe00a47b46ee07beb74e
junior <juniormathiias@gmail.com>
2023-03-23 00:08:18 -0300

    adicionei alguns js e jquery para funcionar

---
+++ laptop.js
@@ -0,0 +1,18 @@
+const select1 = document.getElementById('select01')
+const select2 = document.getElementById('select02')
+const select3 = document.getElementById('select03')
+const select4 = document.getElementById('select04')
+const input = document.getElementById('Resultado')
+
+const ChangeInput = () => {
+    const res = Number(select1.value) + Number(select2.value) + Number(select3.value) + Number(select4.value)
+    input.value = res
+}
+// Quando a página for atualizada o input não ficar vazio
+const res = Number(select1.value) + Number(select2.value) + Number(select3.value) + Number(select4.value)
+input.value = res
+
+select1.onchange = ChangeInput
+select2.onchange = ChangeInput
+select3.onchange = ChangeInput
+select4.onchange = ChangeInput
--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+function arroz(){
+    alert("Arroz");
+}
+
 $(document).ready(function () {
     let errorNome = $("<div class='msg' style='color:red; font-size:12px;'>").text('Nome obrigatório! ');
     let errorEmail = $("<div class='msg' style='color:red; font-size:12px;'>").text('Email obrigatório! ');
